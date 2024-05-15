0cb352aa55c0d0765377613c2e9fa88fbeeb0ba0
sagar nellipudi <sagar.nf@gmail.com>
2024-05-15 17:26:40 +0530

    Functions

--- frontend.sh
+++ frontend.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
 
+check_root
 
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2..... $R Failure $N"
-        exit 1
-    else
-        echo -e "$2.... $G Success $N"
-    fi
-
-}
-
-
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access"
-    exit 1
-else
-    echo "You are super user"
-fi
 
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing nginx"
